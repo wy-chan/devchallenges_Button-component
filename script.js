5d56857b278e4eed78ccc98589cc3338cb0fece9
WY Chan <79743156+wy-chan@users.noreply.github.com>
2022-03-01 19:19:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,59 +5,13 @@ class MyApp extends React.Component{
       currentPage: "Inputs",
       currentBtnId: "nav-inputs"
     }; 
-    this.handlePage=this.handlePage.bind(this);
-    this.showPage=this.showPage.bind(this);
-    this.closeButton=this.closeButton.bind(this);
-    this.showButton=this.showButton.bind(this);
   }
-
-  handlePage(event){
-    this.closeButton(this.state.currentBtnId);
-    this.showButton(event.target.id);
-
-    this.setState({
-      currentPage : event.target.text,
-      currentBtnId: event.target.id
-    });
-    
-    /*this.showPage();*/
-  }
-  /*
-  showPage(){
-    switch(this.state.currentPage){
-      case"Inputs": 
-        return <Inputs/>;
-      break;
-      default: return <Buttons />
-    }
-  }
- */
-  closeButton(id){
-    var e=document.getElementById(id);
-    e.classList.remove("link-active");
-  }
-  showButton(id){
-    var e=document.getElementById(id);
-    e.classList.add("link-active");
-    
-  }
-  
 
   render(){
     return(
-<div id="nav-main-div">
-<nav id="navbar">
-<header><h2><em>Dev</em>challenges.io</h2></header>
-<ul style={{ listStyleType: 'none' }}>
-    <li><a class="nav-link" id="nav-buttons" onClick={this.handlePage}>Buttons</a></li>
-    <li><a class="nav-link link-active" id="nav-inputs" onClick={this.handlePage}>Inputs</a></li>
-</ul>
-</nav>
-<main>
-
-</main>
+<div>
+<h1>Heading</h1>
 </div>
- 
   )
   }
 }
