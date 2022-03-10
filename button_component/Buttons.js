b74ab2e67dfc719fc0c0d2bac096a42e01cc6873
WY Chan <79743156+wy-chan@users.noreply.github.com>
2022-03-11 00:19:36 +0800

    Update Buttons.js

--- button_component/Buttons.js
+++ button_component/Buttons.js
@@ -7,129 +7,129 @@ class Buttons extends React.Component{
     return(
     <section id="buttons-div">
     <h1>Buttons</h1>
-    <div class="button-group">
+    <div className="button-group">
   
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt; Button /&gt;</p> 
      <button class="btn btn-default">Default</button>
      </div>
   
-     <div class="button-box">
-     <p class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-default default-hover">Default</button>
+     <div className="button-box">
+     <p className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-default default-hover">Default</button>
      </div>
   
 </div>
   
-<div class="button-group">
+<div className="button-group">
   
- <div class="button-box">
+ <div className="button-box">
      <p>&lt; Button variant=”outline”/&gt;</p> 
-     <button class="btn btn-outline-primary">Default</button>
+     <button className="btn btn-outline-primary">Default</button>
      </div>
-   <div class="button-box">
-     <p class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-outline-primary primary-focus">Default</button>
+   <div className="button-box">
+     <p className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-outline-primary primary-focus">Default</button>
      </div>
   
   
 </div>
-<div class="button-group">
+<div className="button-group">
   
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button variant=”text” &gt;</p> 
-     <button class="btn btn-link">Default</button>
+     <button className="btn btn-link">Default</button>
      </div>
-   <div class="button-box">
-     <p class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-link link-hover">Default</button>
+   <div className="button-box">
+     <p className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-link link-hover">Default</button>
      </div>
 
 </div>
   
-<div class="button-group">
-     <div class="button-box">
+<div className="button-group">
+     <div className="button-box">
      <p>&lt;Button disableShadow &gt;</p> 
-     <button class="btn btn-primary no-shadow">Default</button>
+     <button className="btn btn-primary no-shadow">Default</button>
      </div>
 </div>
 
-  <div class="button-group">
-     <div class="button-box">
+  <div className="button-group">
+     <div className="button-box">
      <p>&lt;Button disabled / &gt;</p> 
-     <button class="btn btn-default" disabled>Default</button>
+     <button className="btn btn-default" disabled>Default</button>
      </div>
 
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button variant=”text” disabled&gt;</p> 
-     <button class="btn btn-link" disabled>Default</button>
+     <button className="btn btn-link" disabled>Default</button>
      </div>
 </div>
-<div class="button-group">
-     <div class="button-box-L">
+<div className="button-group">
+     <div className="button-box-L">
      <p>&lt;Button startIcon=”local_grocery_store”&gt;</p> 
-     <button class="btn btn-primary btn-icon">
-       <span class="material-icons">local_grocery_store</span>
+     <button className="btn btn-primary btn-icon">
+       <span className="material-icons">local_grocery_store</span>
        Default</button>
      </div>
-     <div class="button-box-L">
+     <div className="button-box-L">
      <p>&lt;Button endIcon=”local_grocery_store”&gt;</p> 
-     <button class="btn btn-primary btn-icon">Default
-       <span class="material-icons">local_grocery_store</span>
+     <button className="btn btn-primary btn-icon">Default
+       <span className="material-icons">local_grocery_store</span>
        </button>
      </div>
 </div>
-<div class="button-group">
-     <div class="button-box">
+<div className="button-group">
+     <div className="button-box">
      <p>&lt;Button size=”sm”&gt;</p> 
-     <button class="btn btn-primary btn-sm">Default</button>
+     <button className="btn btn-primary btn-sm">Default</button>
      </div>
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button size=”md”&gt;</p> 
-     <button class="btn btn-primary btn-md">Default</button>
+     <button className="btn btn-primary btn-md">Default</button>
      </div>
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button size=”lg”&gt;</p> 
-     <button class="btn btn-primary btn-lg">Default</button>
+     <button className="btn btn-primary btn-lg">Default</button>
      </div>
 </div>
-<div class="button-group">
-     <div class="button-box">
+<div className="button-group">
+     <div className="button-box">
      <p>&lt;Button color=”default”&gt;</p> 
-     <button class="btn btn-default">Default</button>
+     <button className="btn btn-default">Default</button>
      </div>
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button color=”primary”&gt;</p> 
-     <button class="btn btn-primary">Default</button>
+     <button className="btn btn-primary">Default</button>
      </div>
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button color=”secondary”&gt;</p> 
-     <button class="btn btn-secondary">Default</button>
+     <button className="btn btn-secondary">Default</button>
      </div>
-     <div class="button-box">
+     <div className="button-box">
      <p>&lt;Button color=”danger”&gt;</p> 
-     <button class="btn btn-danger">Default</button>
+     <button className="btn btn-danger">Default</button>
      </div>
 </div> 
-<div class="button-group">
-     <div class="button-box">
-     <p class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-default default-hover">Default</button>
+<div className="button-group">
+     <div className="button-box">
+     <p className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-default default-hover">Default</button>
      </div>
-     <div class="button-box">
-     <p style={{opacity:"0"}} class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-primary primary-hover">Default</button>
+     <div className="button-box">
+     <p style={{opacity:"0"}} className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-primary primary-hover">Default</button>
      </div>
-     <div class="button-box" >
-     <p style={{opacity:"0"}} class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-secondary secondary-hover">Default</button>
+     <div className="button-box" >
+     <p style={{opacity:"0"}} className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-secondary secondary-hover">Default</button>
      </div>
-     <div class="button-box">
-     <p style={{opacity:"0"}} class="notes">&:hover, &:focus</p> 
-     <button class="btn btn-danger danger-hover">Default</button>
+     <div className="button-box">
+     <p style={{opacity:"0"}} className="notes">&:hover, &:focus</p> 
+     <button className="btn btn-danger danger-hover">Default</button>
      </div>
 </div> 
-<p class="notes">Icons: https://material.io/resources/icons/?style=round</p>
+<p className="notes">Icons: https://material.io/resources/icons/?style=round</p>
 </section>
     )
   }
